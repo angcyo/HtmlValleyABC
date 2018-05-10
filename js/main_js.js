03739a06f52825f3d9cebb3d768a66c5c0977edf
angcyo <angcyo@126.com>
2018-05-10 18:15:48 +0800

    add main html

---
+++ js/main_js.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $(function () {
+        //The passed argument has to be at least a empty object or a object with your desired options
+        $('body').overlayScrollbars({});
+    });
+});
