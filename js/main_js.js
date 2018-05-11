afcdc12aef6d238e2d931657e23e338f3251e6eb
angcyo <angcyo@126.com>
2018-05-11 11:44:58 +0800

    add main nav dialog

--- js/main_js.js
+++ js/main_js.js
@@ -4,10 +4,9 @@ $(document).ready(function () {
         $('body').overlayScrollbars({});
     });
 
-
     $(".bottom_nav_item").click(function () {
         reset_nav(this);
-        selector_nav(this)
+        selector_nav(this);
         // $(this).find("i").addClass("nav_found_selected")
         // $(this).find("span").text("点击")
         // console.log($(this).find("span"))
@@ -17,15 +16,53 @@ $(document).ready(function () {
     })
 
     //默认选中首页
-    $(".bottom_nav_item").first().click()
+    $(".bottom_nav_item").first().click();
+
+    //关闭对话框
+    $(".nav_center_dialog_cancel").click(function () {
+
+        $(".nav_center_dialog").animate({
+            height: '0px',
+        }, function () {
+            //动画结束
+            //$(".nav_center_dialog_wrapper").hide();
+        });
+
+        $(".nav_center_dialog_wrapper").animate({
+            opacity: '0'
+        }, "slow", function () {
+            $(".nav_center_dialog_wrapper").hide();
+        });
+        // alert($(".nav_center_dialog"));
+    });
+
+    $(".nav_center_dialog_wrapper").click(function () {
+        $(".nav_center_dialog_cancel").click();
+    });
+
+    //显示对话框
+    $(".nav_center").click(function () {
+        //alert($(".nav_center_dialog"));
+
+        $(".nav_center_dialog_wrapper").show();
+        $(".nav_center_dialog_wrapper").animate({
+            opacity: '0.5'
+        }, "slow");
+
+        $(".nav_center_dialog").animate({
+            height: '180px',
+        });
+    })
 });
 
 /*重置底部导航栏默认图标*/
 function reset_nav(nav_item) {
-    //$(nav_item).parent().find("i").removeClass();
-    // console.log($(nav_item).parent().find("a"));
+    let ico = $(nav_item).find("i");
+    if ($(ico).length <= 0) {
+        //选中了一个没有i标签的nav
+        return;
+    }
     $(nav_item).parent().find("a").each(function () {
-        //console.log("->" + $(this).attr("id"));
         let ico = $(this).find("i");
         ico.removeClass();
         switch ($(this).attr("id")) {
@@ -48,6 +85,9 @@ function reset_nav(nav_item) {
 /*选中导航*/
 function selector_nav(nav_item) {
     let ico = $(nav_item).find("i");
+    if ($(ico).length <= 0) {
+        return;
+    }
     ico.removeClass();
     switch ($(nav_item).attr("id")) {
         case "nav_home":
